fa812fba46888d8b557e1d3fa1343954ed1de400
PRASHANT GIRI <104084695+pg570@users.noreply.github.com>
2022-10-02 11:05:58 +0530

    Update projects.router.js

--- src/features/projects/projects.router.js
+++ src/features/projects/projects.router.js
@@ -27,7 +27,7 @@ app.get("/:id",async (req,res)=>{
 app.get("/:id",async (req,res)=>{
     try{
         let id = req.params.id;
-        let project = await Projects.find({"userId": id});
+        let project = await Projects.find(id);
         res.send(project);
     }catch(e){
         res.status(500).send(e);
