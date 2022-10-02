bc45bb918ee8bebd2c8dcf61e2dcdd745d3f72a0
PRASHANT GIRI <104084695+pg570@users.noreply.github.com>
2022-10-02 10:42:58 +0530

    Update projects.model.js

--- src/features/projects/projects.model.js
+++ src/features/projects/projects.model.js
@@ -1,6 +1,7 @@
 const mongoose =require("mongoose");
 
 const projectSchema = mongoose.Schema({
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"user",required:true},
     clientId:{type:mongoose.Schema.Types.ObjectId,ref:"client",required:true},
     name:{type:String,required:true},
     currency:{type:String},
